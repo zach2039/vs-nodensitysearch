8538bf0b49c45d880657a4d46a6b220644e86f84
Zach2039 <sumyunguy2007@gmail.com>
2024-05-15 20:15:04 -0500

    Fix naming (oops)

--- NoDensitySearch/ModTemplateModSystem.cs
+++ NoDensitySearch/ModTemplateModSystem.cs
@@ -3,9 +3,9 @@ using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
 
-namespace NoNodeSearch
+namespace NoDensitySearch
 {
-    public class NoNodeSearchModSystem : ModSystem
+    public class NoDensitySearchModSystem : ModSystem
     {
         // Called on server and client
         public override void Start(ICoreAPI api)
